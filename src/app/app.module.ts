bb7895c73b4c4ae304386f6d09cd0f6546618374
Alberth Salas C <alberthsalascalero@gmail.com>
2018-06-18 16:08:11 -0600

    Inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {'path': '**', component: MainComponent}
+    ]),
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+// Las puntuaciones deben ir de -1 (negativo) a 1 (positiva), siendo 0 una puntuación neutral
+interface NLPRES {
+  score: Number;
+  keyWords: String[];
+  keyScores: {key: String, value: Number};
+}
+class NLP {
+  constructor() {}
+  google(text) {
+    const keyScores = <{key: String, value: Number}> {};
+    const keyWords = <String[]> [];
+    const score = <Number> 0;
+    return <NLPRES> {score: score, keyWords: keyWords, keyScores: keyScores};
+  }
+}
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+  private texto = '';
+  private score = 0;
+  private keyWords = [];
+  private keyScores = [];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
