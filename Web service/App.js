032a1366ba759649fb1e2107f234bf390777e000
Alberth Salas C <alberthsalascalero@gmail.com>
2018-06-20 14:03:23 -0600

    nodemon

--- Web service/App.js
+++ Web service/App.js
@@ -3,15 +3,18 @@ var app = express();
 var http =  require('http');
 
 
+var AWS = require('aws-sdk');
+const glanguage = require('@google-cloud/language');
+const cognitiveServices = require('cognitive-services');
+const AYLIENTextAPI = require('aylien_textapi');
+const credentials = require('./API_KEYS').API_KEYS;
+const request = require('request');
+
 /************************************************
 Getting data for AWS Comprehend configuration
 *************************************************/
 
 var language="en";
-
-var AWS = require('aws-sdk');
- const glanguage = require('@google-cloud/language');
-var credentials = require('./API_KEYS').API_KEYS;
 
 
 /****************************************
@@ -26,7 +29,6 @@ var AmazonNLP = new AWS.Comprehend({apiVersion: credentials.amazon.API_version})
 /****************************************
  Azure text analysis service import and configuration
 ******************************************/
-const cognitiveServices = require('cognitive-services');
 const AzureNLP = new cognitiveServices.textAnalytics({
     apiKey: credentials.azure.azureFirstKey,
     endpoint: credentials.azure.azureEndpoint
@@ -38,7 +40,6 @@ AYLIENT TEXT ANALYSIS SERVICE
 
 *********************************************************/
 
-const AYLIENTextAPI = require('aylien_textapi');
 var textapi = new AYLIENTextAPI({
   application_id: credentials.Aylien.appID,
   application_key: credentials.Aylien.appKey
@@ -71,8 +72,8 @@ app.get('/amazonComprehend',function(req,res){
         {
         		//format response
         		var respuesta={};
-        		respuesta["score"]=0;
-        		respuesta["keyScores"]=[];
+        		respuesta.score=0;
+        		respuesta.keyScores=[];
         		var limite=data.KeyPhrases.length;
         		var keyPhrases= data.KeyPhrases;
         		for (let i=0; i< limite;i++){
@@ -106,8 +107,8 @@ app.get('/azureCognitiveService',function(req,res){
 		.then((response) => {
 			//Format response
 			var respuesta={};
-            respuesta["score"]=0;
-            respuesta["keyScores"]=[];
+            respuesta.score=0;
+            respuesta.keyScores=[];
             //azure let us to analize many documents at the same time, but now we are working with one
             var documentsSize=response.documents.length;
             var keyPhrasesAmount;
@@ -130,83 +131,56 @@ app.get('/azureCognitiveService',function(req,res){
 			console.log(err);
 			res.end(JSON.stringify({"success":false,"data": []}));
             });
-
-	/*
-	AzureNLP.sentiment({headers,body})
-		.then((response) => {
-			res.end(JSON.stringify({"success":true,"data": response}));
-       		})
-		.catch((err) => {
-            console.log(err);
-            res.end(JSON.stringify({"success":false,"data": []}));
-            });
-	*/
-
 });
 
 app.get('/aylienTextApi',function(req,res){
-
     textapi.entities(req.query.text, function(err, resp) {
-    
   		if (err !== null) {
     		res.send(JSON.stringify({}));
   			} 
   		else 
   		{
-  			
   			//format response
             var respuesta={};
-            respuesta["score"]=0;
-            respuesta["keyScores"]=[];
+            respuesta.score=0;
+            respuesta.keyScores=[];
             var limite=resp.entities.keyword.length;
             var keyPhrases= resp.entities.keyword;
-                		
-
             for (let i=0; i< limite;i++){
             	respuesta.keyScores.push(
                 	{
-                		"key": keyPhrases[i],
-                		"value": 0 
-
-                	}
-                	);
+                        "key": keyPhrases[i],
+                        "value": 0
+                	});
             }
-			
             res.send(JSON.stringify(respuesta));
     	}
-  		
 	});
-    
-
 });
-
-
 
 app.get('/googleLanguage',function(req,res){
   const document = {
     content: req.query.text,
     type: 'PLAIN_TEXT',
   };
-
   // Detects the sentiment of the text
   GoogleNLP
     .analyzeEntitySentiment({document: document})
     .then(results => {
       const sentiment = results[0].entities;
-      var respuesta= {}
-      respuesta["score"] = 0;
-      respuesta["keyWords"] = [];
-      respuesta["keyScores"] = [];
+      var respuesta= {};
+      respuesta.score = 0;
+      respuesta.keyScores = [];
       for(let x = 0; sentiment[x]!=null; x++){
-        respuesta.keyScores.push({key: sentiment[x].name, value:sentiment[x].sentiment.score})
+        respuesta.keyScores.push({key: sentiment[x].name, value:sentiment[x].sentiment.score});
       }
       GoogleNLP
         .analyzeSentiment({document: document})
         .then(results => {
           const sentiment = results[0].documentSentiment;
           respuesta.score = sentiment.score;
-          res.send(respuesta)
-        })
+          res.send(respuesta);
+        });
     })
     .catch(err => {
       console.error('ERROR:', err);
@@ -214,7 +188,6 @@ app.get('/googleLanguage',function(req,res){
 });
 
 app.get('/IBMWatson', function(req,res){
-  var request = require('request');
   var headers = {
       'Content-Type': 'application/json'
   };
@@ -239,13 +212,11 @@ app.get('/IBMWatson', function(req,res){
   };
   function callback(error, response, body) {
       if (!error && response.statusCode == 200) {
-          var watsy = JSON.parse(body)
-          var respuesta= {}
-          respuesta["score"] = watsy.sentiment.document.score;
-          respuesta["keyWords"] = [];
-          respuesta["keyScores"] = [];
+          var watsy = JSON.parse(body);
+          var respuesta= {};
+          respuesta.score = watsy.sentiment.document.score;
+          respuesta.keyScores = [];
           for (let x=0;watsy.keywords[x]!=undefined;x++){
-            respuesta.keyWords.push(watsy.keywords[x].text);
             respuesta.keyScores.push({
               key: watsy.keywords[x].text,
               value: watsy.keywords[x].sentiment.score});
@@ -254,8 +225,7 @@ app.get('/IBMWatson', function(req,res){
       }
   }
   request(options, callback);
-})
-
+});
 
 var server = app.listen(8081, function ()
 {
--- Web service/RUN SERVER.bat
+++ Web service/RUN SERVER.bat
@@ -1,3 +1,3 @@
 set GOOGLE_APPLICATION_CREDENTIALS=API NLP Context-914a104bdc50.json
-node app.js
+nodemon app.js
 pause
